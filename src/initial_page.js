bcdf6edcf5a0344117426b226c8273ba64973348
DavedCV <97773641+DavedCV@users.noreply.github.com>
2023-08-17 11:47:11 -0500

    - Deleted the html structure of the header, and create this via javascript in the module 'initial_page.js' - Import the module into the 'index.js' - Bundle into 'main.js' - Add comments to the CSS header styles

--- src/initial_page.js
+++ src/initial_page.js
@@ -1,27 +1,98 @@
+/* ----------------------------- header --------------------------------------*/
+
+const content = document.querySelector("#content");
+
+const header = document.createElement("header");
+header.classList.add("header");
+header.setAttribute("data-header", "");
+
+const headerContainer = document.createElement("div");
+headerContainer.classList.add("container");
+
+// create the logo elements
+const headerH1 = document.createElement("h1");
+const logo = document.createElement("a");
+logo.textContent = "Restaurant";
+logo.classList.add("logo");
+
+const spanLogo = document.createElement("span");
+spanLogo.textContent = ".";
+spanLogo.classList.add("span");
+
+headerH1.appendChild(logo);
+logo.appendChild(spanLogo);
+
+// create the navbar
+const headerNavbar = document.createElement("nav");
+headerNavbar.setAttribute("data-navbar", "");
+headerNavbar.classList.add("navbar");
+
+const navbarList = document.createElement("ul");
+const navbarOptions = ["Home", "About", "Menu", "Contact"];
+const navbarLinks = [];
+for (let option of navbarOptions) {
+  const li = document.createElement("li");
+  li.classList.add("nav-item");
+
+  const a = document.createElement("a");
+  a.setAttribute("href", `#${option.toLowerCase()}`);
+  a.setAttribute("data-nav-link", "");
+  a.classList.add("navbar-link");
+  a.textContent = option;
+  navbarLinks.push(a);
+
+  li.appendChild(a);
+  navbarList.appendChild(li);
+}
+
+headerNavbar.appendChild(navbarList);
+
+// create the buttons group
+const headerButtonGroup = document.createElement("div");
+headerButtonGroup.classList.add("header-btn-group");
+
+const buttonNav = document.createElement("button");
+buttonNav.classList.add("nav-toggle-btn");
+buttonNav.setAttribute("data-menu-toggle-btn", "");
+
+for (let i = 0; i < 3; i++) {
+  const pos = i == 0 ? "top" : i == 1 ? "middle" : "bottom";
+  const span = document.createElement("span");
+  span.setAttribute("class", `line ${pos}`);
+  buttonNav.appendChild(span);
+}
+
+headerButtonGroup.appendChild(buttonNav);
+
+// append all sections to the header main element
+headerContainer.appendChild(headerH1);
+headerContainer.appendChild(headerNavbar);
+headerContainer.appendChild(headerButtonGroup);
+header.appendChild(headerContainer);
+
+// add to the content
+
+content.appendChild(header);
+
+/* ----------------------------- dom listeners -------------------------------*/
+
 // navbar toggle
 
-const navbar = document.querySelector("[data-navbar]");
-const navbarLinks = [...document.querySelectorAll("[data-nav-link]")];
-const menuToggleButton = document.querySelector("[data-menu-toggle-btn]");
-
-menuToggleButton.addEventListener("click", function () {
-  navbar.classList.toggle("active");
+buttonNav.addEventListener("click", function () {
+  headerNavbar.classList.toggle("active");
   this.classList.toggle("active");
 });
 
-navbar.addEventListener("click", function (e) {
-  console.log(this);
+headerNavbar.addEventListener("click", function (e) {
   if (navbarLinks.includes(e.target)) {
-    navbar.classList.toggle("active");
-    menuToggleButton.classList.toggle("active");
+    headerNavbar.classList.toggle("active");
+    buttonNav.classList.toggle("active");
   }
 });
 
 // header sticky
 
-const header = document.querySelector("[data-header]");
-
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
   if (window.scrollY >= 100) header.classList.add("active");
   else header.classList.remove("active");
 });
